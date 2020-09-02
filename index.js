0ded5cdc8b6bba92f5c50e6bcf258dc617b15c1a
skandow <kando1sr@gmail.com>
2020-09-02 10:34:33 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
+function findMinAndRemoveSorted(array) {
+    return array.shift()
+}
+
+function merge(array1, array2) {
+    let sorted = []
+    while ((array1.length !== 0) && (array2.length !== 0)) {
+        if (array1[0] < array2[0]) {
+            sorted.push(findMinAndRemoveSorted(array1))
+        } else {
+            sorted.push(findMinAndRemoveSorted(array2))
+        }
+    }
+    return sorted.concat(array1).concat(array2)
+}
+
+function mergeSort(array) {
+    let midpoint = array.length / 2
+    let array1 = array.slice(0, midpoint)
+    let array2 = array.slice(midpoint)
+
+    if (array.length < 2) {
+        return array
+    } else {
+        return merge(mergeSort(array1), mergeSort(array2))
+    }
+}
 
